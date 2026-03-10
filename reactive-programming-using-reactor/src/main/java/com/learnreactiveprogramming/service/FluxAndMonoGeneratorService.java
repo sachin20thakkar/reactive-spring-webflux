ad66823e58fa769452211b2d86171af8eaf06e00
Sachin Thakkar <sachin20thakkar@gmail.com>
2026-03-10 09:59:00 +0530

    First commit for Flux and Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,25 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(s -> System.out.println("Name is "+ s));
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(s -> System.out.println("Mono name is " + s));
+    }
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("alex");
+    }
 }
